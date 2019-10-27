6ee4602e52eecb82dd278c67397eddb28633ee8a
Aliaksandr Kananenka <Aliaksandr_Kananenka@epam.com>
2019-10-27 19:33:09 +0300

    Webpack dev server was added. Index.html replace to dist folder.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 
 module.exports = {
+    mode: "development",
     entry: {
         app: ["babel-polyfill", "./root/src/index.js"]
     },
     output: {
         path: path.resolve(__dirname, 'root/dist'),
         filename:'main.js',
-        publicPath: 'http://localhost:3000/'
+        publicPath: 'http://localhost:8080/'
     },
     module: {
         rules: [
@@ -28,5 +29,10 @@ module.exports = {
                 use: ["style-loader", "css-loader"],
             },
         ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'root/dist'),
+        compress: true,
+        port: 8080
     },
 };
